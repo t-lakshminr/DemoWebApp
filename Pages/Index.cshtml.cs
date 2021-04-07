9f3316c0a428e4bd287d65a5e0b67653a5693d55
Lakshmi N R <t-lakshminr@microsoft.com>
2021-04-07 21:02:01 +0530

    Added 2 components related to Httpconnections and System.text

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http.Connections;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Text.Encodings.Web;
 
 namespace DemoWebApp.Pages
 {
